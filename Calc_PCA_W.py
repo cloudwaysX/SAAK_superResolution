63b46b35c1c22de63a86c9946a16460f55b81ce8
cloudwaysX <yifang@usc.edu>
2017-09-23 23:08:23 -0700

    add dataset and non-full PCA

--- Calc_PCA_W.py
+++ Calc_PCA_W.py
@@ -7,22 +7,6 @@ Created on Tue Sep 12 21:13:01 2017
 """
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 import time
 tic = time.clock()
 
@@ -31,8 +15,13 @@ from sklearn.decomposition import PCA
 from skimage.util.shape import view_as_windows
 from sklearn import preprocessing
 
+def CalcNextChannelNum(preOutNum, keepComp=1):
+    nextL_in = preOutNum*2-1
+    nextL_out = int(round(nextL_in*4*keepComp))+1
+    return nextL_in,nextL_out
 
-def Cal_W_PCA(X,reception_size = 2,stride = (2,2,3)):
+def Cal_W_PCA(X,n_keptComponent,reception_size = 2,stride = (2,2,3)):
+#    print(n_keptComponent)
     assert len(X.shape)==4, "input images batch is not 4D"
     X=np.transpose(X,(2,3,0,1)) # width,height,sampleNum,depth
     assert X.shape[0] == X.shape[1], "input image is not square"
@@ -45,7 +34,7 @@ def Cal_W_PCA(X,reception_size = 2,stride = (2,2,3)):
     #(input_width/stride,input_width/stride,1,reception_size,reception_size,input_depth)
     X = np.reshape(X,(input_width,input_width,input_depth*sampleNum)) #stack all samples alonge depth axis
     X_aranged = view_as_windows(X, window_shape=(reception_size, reception_size,input_depth),step=stride)
-#    print(X_aranged.shape)
+
     # rearranged the extracted patches stacks
     # shape is (n_samples, n_features)
     patchNum = ((input_width-reception_size)/stride[0]+1)**2*sampleNum
@@ -63,18 +52,21 @@ def Cal_W_PCA(X,reception_size = 2,stride = (2,2,3)):
 #
 # =============================================================================
     X_aranged_whiten = X_aranged
-    #########################################
+    ############### AC ####################
     #apply PCA projection on the extracted patch samples
     #n_components == min(n_samples, n_features)
     pca = PCA(n_components=X_aranged_whiten.shape[1],svd_solver='full')
     pca.fit(X_aranged_whiten)
     #shape (n_components, n_features)
     W_pca_aranged = pca.components_
-#    print(W_pca_aranged)
+    W_pca_aranged = W_pca_aranged[:n_keptComponent]
+
+    ############### DC ######################
+    W_pca_aranged = np.concatenate( (np.ones((1,W_pca_aranged.shape[1]))/W_pca_aranged.shape[1], W_pca_aranged) )
 
     #shape as convolution weight required
     #shape: (output_depth,reception_size,reception_size,put_depth,)
-    W_pca = np.reshape(W_pca_aranged,(input_depth*reception_size*reception_size,reception_size,reception_size,input_depth))
+    W_pca = np.reshape(W_pca_aranged,(n_keptComponent+1,reception_size,reception_size,input_depth))
     #shape: (output_depth,input_depth,reception_size,reception_size)
     W_pca = np.transpose(W_pca,(0,3,1,2))
     
@@ -104,10 +96,74 @@ def Cal_W_PCA(X,reception_size = 2,stride = (2,2,3)):
 
 #X = np.arange(3*8*8,dtype=float).reshape(3,8,8)
 #X = np.stack((X,X))
-X = np.random.randn(1000,3,32,32)*100
-print(X[0,:,:,:])
-#X=np.random.rand(3,8,8)*10
+#import os
+#from scipy import misc
+#import matplotlib.pyplot as plt
+#
+#
+#def readImg(path,name):
+#    image= misc.imread(os.path.join(path,name), flatten= 0)
+#    return image
+#
+#path = '/home/yifang/SAAK_superResolution/SRCNN/Set5'
+#name = 'baby_GT.bmp'
+#
+#img = readImg(path,name)
+#
+#img_resized = misc.imresize( img, (32,32,3),interp = 'cubic')
+#X = np.tile(img_resized,(1000,1,1,1))
+#X = np.transpose(X, (0,3,1,2))
+#X = np.float32(X)
+def load_databatch(data_folder, idx, img_size=32):
+    import os
+    import pickle
+    import numpy as np
+    import lasagne
+    
+    def unpickle(name):
+        with open(name, 'rb') as pickle_file:
+            content = pickle.load(pickle_file)
+            return content
+    
+    
+    data_file = os.path.join(data_folder, 'train_data_batch_')
 
+    d = unpickle(data_file + str(idx))
+    x = d['data']
+    y = d['labels']
+    mean_image = d['mean']
+
+    x = x/np.float32(255)
+    mean_image = mean_image/np.float32(255)
+
+    # Labels are indexed from 1, shift it so that indexes start at 0
+    y = [i-1 for i in y]
+    data_size = x.shape[0]
+
+    x -= mean_image
+
+    img_size2 = img_size * img_size
+
+    x = np.dstack((x[:, :img_size2], x[:, img_size2:2*img_size2], x[:, 2*img_size2:]))
+    x = x.reshape((x.shape[0], img_size, img_size, 3)).transpose(0, 3, 1, 2)
+
+    # create mirrored images
+    X_train = x[0:data_size, :, :, :]
+    Y_train = y[0:data_size]
+    X_train_flip = X_train[:, :, :, ::-1]
+    Y_train_flip = Y_train
+    X_train = np.concatenate((X_train, X_train_flip), axis=0)
+    Y_train = np.concatenate((Y_train, Y_train_flip), axis=0)
+
+    return dict(
+        X_train=lasagne.utils.floatX(X_train),
+        Y_train=Y_train.astype('int32'),
+        mean=mean_image)
+
+data_folder = '/home/yifang/SAAK_superResolution/data/train'
+the_batch = load_databatch(data_folder,1)
+X=the_batch['X_train']+np.reshape(the_batch['mean'],(3,32,32))
+X = X[:1000]
 
 import torch
 from torch.autograd import Variable
@@ -121,186 +177,90 @@ class Net(nn.Module):
         super(Net, self).__init__()
         # 1 input image channel, 6 output channels, 5x5 square convolution
         # kernel
-        self.conv1 = nn.Conv2d(3, 12, 2,stride=2 ) 
-        self.conv2 = nn.Conv2d(24, 96, 2,stride=2)
-        self.conv3 = nn.Conv2d(192, 768, 2,stride=2)
-        self.conv4 = nn.Conv2d(192*8, 768*8, 2,stride=2)
-        self.conv5 = nn.Conv2d(192*64, 768*64, 2,stride=2)
+        keepComp_init = 0.4
+        curL_in = 3; curL_out = int(3*4*keepComp_init) + 1 # initial
+        self.conv1 = nn.Conv2d(curL_in, curL_out, 2,stride=2 ) #full 3,13
+        curL_in,curL_out = CalcNextChannelNum(curL_out,keepComp=0.4)
+        self.conv2 = nn.Conv2d(curL_in, curL_out, 2,stride=2)#full 24+1, 100+1
+        curL_in,curL_out = CalcNextChannelNum(curL_out,keepComp=0.4)
+        self.conv3 = nn.Conv2d(curL_in, curL_out, 2,stride=2)#full 200+1, 804+1
+        curL_in,curL_out = CalcNextChannelNum(curL_out,keepComp=0.4)
+        self.conv4 = nn.Conv2d(curL_in, curL_out, 2,stride=2)#full 804*2+1, (804*2+1)*4+1
+        curL_in,curL_out = CalcNextChannelNum(curL_out,keepComp=0.4)
+        self.conv5 = nn.Conv2d(curL_in, curL_out, 2,stride=2) # full 804*2+1, (804*2+1)*4+1
 
     def forward1(self, x):
         z1 = self.conv1(x)
-        z1 = torch.cat((z1,-z1),dim=1)
+        z1 = torch.cat((z1,-z1[:,1:,:,:]),dim=1)
         A1 = F.relu(z1)
         return A1
     
     def forward2(self,A1):
         z2 = self.conv2(A1)
-        z2 = torch.cat((z2,-z2),dim=1)
+        z2 = torch.cat((z2,-z2[:,1:,:,:]),dim=1)
         A2 = F.relu(z2)
         return A2
         
     def forward3(self,A2):
         z3 = self.conv3(A2)
-        z3 = torch.cat((z3,-z3),dim=1)
+        z3 = torch.cat((z3,-z3[:,1:,:,:]),dim=1)
         A3 = F.relu(z3)
         return A3
     
     def forward4(self,A3):
         z4 = self.conv4(A3)
-        z4 = torch.cat((z4,-z4),dim=1)
+        z4 = torch.cat((z4,-z4[:,1:,:,:]),dim=1)
         A4 = F.relu(z4)
+        A4 = np.cat((z4[:,0,:,:],A4),dim=1)
         return A4
     
     def forward5(self,A4):
         z5 = self.conv5(A4)
-        z5 = torch.cat((z5,-z5),dim=1)
+        z5 = torch.cat((z5,-z5[:,1:,:,:]),dim=1)
         A5 = F.relu(z5)
         return A5
+
     
     
 net = Net()
-print('processing A1 ...')
-#W_pca, W_pca_INV, X_proj_reference, X_proj_INV_reference= Cal_W_PCA(X)
-W_pca1= Cal_W_PCA(X)
-#store the weight
-np.save('/home/yifang/SAAK_superResolution/PCA_weight/PCA_weight1_12_3_2_2.npy',W_pca1)
 # wrap input as variable
 X=torch.Tensor(X)
 X=Variable(X)
+print('processing A1 ...')
+#W_pca, W_pca_INV, X_proj_reference, X_proj_INV_reference= Cal_W_PCA(X)
+curInCha,curOutCha = net.conv1.weight.size()[1],net.conv1.weight.size()[0]
+W_pca1= Cal_W_PCA(X.data.numpy(),curOutCha-1,stride = (2,2,curInCha))
+#store the weight
+np.save('/home/yifang/SAAK_superResolution/PCA_weight/PCA_weight1_12_3_2_2_04.npy',W_pca1)
 net.conv1.weight.data=torch.Tensor(W_pca1)
 net.conv1.bias.data.fill_(0)
 A1 = net.forward1(X)
 
 print('processing A2 ...')
-W_pca2= Cal_W_PCA(A1.data.numpy(),stride = (2,2,24))
+curInCha,curOutCha = net.conv2.weight.size()[1],net.conv2.weight.size()[0]
+W_pca2= Cal_W_PCA(A1.data.numpy(),curOutCha-1,stride = (2,2,curInCha))
 #store the weight
-np.save('/home/yifang/SAAK_superResolution/PCA_weight/PCA_weight1_96_24_2_2.npy',W_pca2)
+np.save('/home/yifang/SAAK_superResolution/PCA_weight/PCA_weight1_96_24_2_2_04.npy',W_pca2)
 net.conv2.weight.data=torch.Tensor(W_pca2)
 net.conv2.bias.data.fill_(0)
 A2 = net.forward2(A1)
 
 print('processing A3 ...')
-W_pca3= Cal_W_PCA(A2.data.numpy(),stride = (2,2,192))
-np.save('/home/yifang/SAAK_superResolution/PCA_weight/PCA_weight1_768_192_2_2.npy',W_pca3)
+curInCha,curOutCha = net.conv3.weight.size()[1],net.conv3.weight.size()[0]
+W_pca3= Cal_W_PCA(A2.data.numpy(),curOutCha-1,stride = (2,2,curInCha))
+np.save('/home/yifang/SAAK_superResolution/PCA_weight/PCA_weight1_768_192_2_2_04.npy',W_pca3)
 net.conv3.weight.data=torch.Tensor(W_pca3)
 net.conv3.bias.data.fill_(0)
 A3 = net.forward3(A2)
 
 #print('processing A4 ...')
-#W_pca4= Cal_W_PCA(A3.data.numpy(),stride = (2,2,192*8))
+#curInCha,curOutCha = net.conv4.weight.size()[1],net.conv4.weight.size()[0]
+#W_pca4= Cal_W_PCA(X.data.numpy(),curOutCha-1,stride = (2,2,curInCha))
 #net.conv4.weight.data=torch.Tensor(W_pca4)
 #net.conv4.bias.data.fill_(0)
 #A4 = net.forward4(A3)
 
 
 
-class Inv_Net(nn.Module):
 
-    def __init__(self):
-        super(Inv_Net, self).__init__()
-        # 1 input image channel, 6 output channels, 5x5 square convolution
-        # kernel
-        self.deconv1 = nn.ConvTranspose2d(12,3,2,stride=2 )
-        self.deconv2 = nn.ConvTranspose2d(96, 24, 2,stride=2)
-        self.deconv3 = nn.ConvTranspose2d(768, 192, 2,stride=2)
-        self.deconv4 = nn.ConvTranspose2d(768*8, 768, 2,stride=2)
-        self.deconv5 = nn.ConvTranspose2d(768*64, 768*8, 2,stride=2)
 
-    def forward1_inv(self, A1):
-        z1 = A1[:,0:12,:,:]-A1[:,12:24,:,:] 
-        x = self.deconv1(z1)
-        return x
-        
-    def forward2_inv(self, A2):
-        z2 = A2[:,0:96,:,:]-A2[:,96:192,:,:] 
-        A1 = self.deconv2(z2)
-        return A1
-    
-    def forward3_inv(self,A3):
-        z3 = A3[:,0:768,:,:]-A3[:,768:768*2,:,:] 
-        A2 = self.deconv3(z3)
-        return A2
-    
-    def forward4_inv(self,A4):
-        z4 = A4[:,0:768*8,:,:]-A4[:,768*8:768*2*8,:,:] 
-        A3 = self.deconv4(z4)
-        return A3    
-    
-    def forward5_inv(self,A5):
-        z5 = A5[:,0:768*64,:,:]-A5[:,768*64:768*2*64,:,:] 
-        A4 = self.deconv5(z5)
-        return A4    
-    
-net_inv = Inv_Net()
-
-#A4_back = A4
-#net_inv.deconv4.weight.data=torch.Tensor(W_pca4)
-#net_inv.deconv4.bias.data.fill_(0)
-#A3_back = net_inv.forward4_inv(A4_back)
-
-A3_back=A3
-print('processing A3 back to A2 ...')
-net_inv.deconv3.weight.data=torch.Tensor(W_pca3)
-net_inv.deconv3.bias.data.fill_(0)
-A2_back = net_inv.forward3_inv(A3_back)
-
-print('processing A2 back to A1...')
-net_inv.deconv2.weight.data=torch.Tensor(W_pca2)
-net_inv.deconv2.bias.data.fill_(0)
-A1_back = net_inv.forward2_inv(A2_back)
-
-print('processing A1 back to x ...')
-net_inv.deconv1.weight.data=torch.Tensor(W_pca1)
-net_inv.deconv1.bias.data.fill_(0)
-result_back = net_inv.forward1_inv(A1_back)
-print(result_back.data[0,:,:,:])
-#
-    
-
-toc = time.clock()
-
-print(toc-tic)
-
-#import torch
-#import torchvision
-#import torchvision.transforms as transforms
-
-################## Download cifar-10
-#transform = transforms.Compose(
-#    [transforms.ToTensor(),
-#     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-#
-#trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-#                                        download=True, transform=transform)
-#trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
-#                                          shuffle=True, num_workers=2)
-#
-#testset = torchvision.datasets.CIFAR10(root='./data', train=False,
-#                                       download=True, transform=transform)
-#testloader = torch.utils.data.DataLoader(testset, batch_size=4,
-#                                         shuffle=False, num_workers=2)
-#
-#classes = ('plane', 'car', 'bird', 'cat',
-#           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-#
-#import matplotlib.pyplot as plt
-#import numpy as np
-#
-## functions to show an image
-#
-#
-#def imshow(img):
-#    img = img / 2 + 0.5     # unnormalize
-#    npimg = img.numpy()
-#    plt.imshow(np.transpose(npimg, (1, 2, 0)))
-#    
-#
-#
-## get some random training images
-#dataiter = iter(trainloader)
-#images, labels = dataiter.next()
-#
-## show images
-#imshow(torchvision.utils.make_grid(images))
-## print labels
-#print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
--- Conv_invConv_net.py
+++ Conv_invConv_net.py
@@ -21,10 +21,53 @@ parser.add_argument('--weight_INT', type=str, default='pca', metavar='N',
 
 args = parser.parse_args()
 
+
+
+#X = np.arange(3*8*8,dtype=float).reshape(3,8,8)
+#X = np.stack((X,X))
+import os
+from scipy import misc
+import matplotlib.pyplot as plt
+
+
+def readImg(path,name):
+    image= misc.imread(os.path.join(path,name), flatten= 0)
+    return image
+
+def showImg(outBatch,index=0,title="inverse Rsult"):
+    # input: outBatch with size (imageNum, imageDepth, ImageW, ImageH)
+    # index = the index of the image you want to show; tiile = string of plot title
+    displayImg = outBatch[index,:,:,:].data.numpy()
+    displayImg=np.uint8(displayImg)
+    plt.figure()    
+    plt.title(title)
+    plt.imshow(np.transpose(displayImg, (1,2,0)))
+    plt.show()
+    plt.figure()   
+
+def showFeatureVec(outFeatureVec,index=0,title = "Feature Vec"):
+    from pylab import stem
+    x_axis = range(0,A3.size()[1])
+    rowNum = A3.size()[2]; colNum=A3.size()[3]
+    plt.figure()    
+    plt.title(title)
+    for row in range(0,rowNum):
+        for col in range(0,colNum):
+            subplot_I= row*colNum+col+1
+            plt.subplot(rowNum,colNum,subplot_I)
+            stem(x_axis, outFeatureVec[0,:,row,col].data.numpy())
+    
+    
 import numpy as np
 from sklearn.decomposition import PCA
 from skimage.util.shape import view_as_windows
 from sklearn import preprocessing
+
+def CalcNextChannelNum(preOutNum, keepComp=1):
+    nextL_in = preOutNum*2-1
+    nextL_out = int(round(nextL_in*4*keepComp))+1
+    return nextL_in,nextL_out
+    
 
 def Cal_inv_W(W,reception_size = 2,stride = (2,2,3)):
     # W has shape (outputChannel, inputChannel, receptionSize, receptionSize)
@@ -56,11 +99,18 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-#X = np.arange(3*8*8,dtype=float).reshape(3,8,8)
-#X = np.stack((X,X))
-X = np.random.randn(1,3,8,8)
-print(X)
+ 
 
+
+path = '/home/yifang/SAAK_superResolution/SRCNN/Set5'
+name = 'baby_GT.bmp'
+
+img = readImg(path,name)
+
+img_resized = misc.imresize( img, (32,32,3),interp = 'cubic')
+X = np.stack((img_resized,img_resized))
+X = np.transpose(X, (0,3,1,2))
+X = np.float32(X)
 
 #X=np.random.rand(3,8,8)*10
 #W_pca, W_pca_INV, X_proj_reference, X_proj_INV_reference= Cal_W_PCA(X)
@@ -72,39 +122,46 @@ class Net(nn.Module):
         super(Net, self).__init__()
         # 1 input image channel, 6 output channels, 5x5 square convolution
         # kernel
-        self.conv1 = nn.Conv2d(3, 12, 2,stride=2 ) 
-        self.conv2 = nn.Conv2d(24, 96, 2,stride=2)
-        self.conv3 = nn.Conv2d(192, 768, 2,stride=2)
-        self.conv4 = nn.Conv2d(192*8, 768*8, 2,stride=2)
-        self.conv5 = nn.Conv2d(192*64, 768*64, 2,stride=2)
+        keepComp_init = 0.4
+        curL_in = 3; curL_out = int(3*4*keepComp_init) + 1 # initial
+        self.conv1 = nn.Conv2d(curL_in, curL_out, 2,stride=2 ) #full 3,13
+        curL_in,curL_out = CalcNextChannelNum(curL_out,keepComp=0.4)
+        self.conv2 = nn.Conv2d(curL_in, curL_out, 2,stride=2)#full 24+1, 100+1
+        curL_in,curL_out = CalcNextChannelNum(curL_out,keepComp=0.4)
+        self.conv3 = nn.Conv2d(curL_in, curL_out, 2,stride=2)#full 200+1, 804+1
+        curL_in,curL_out = CalcNextChannelNum(curL_out,keepComp=0.4)
+        self.conv4 = nn.Conv2d(curL_in, curL_out, 2,stride=2)#full 804*2+1, (804*2+1)*4+1
+        curL_in,curL_out = CalcNextChannelNum(curL_out,keepComp=0.4)
+        self.conv5 = nn.Conv2d(curL_in, curL_out, 2,stride=2) # full 804*2+1, (804*2+1)*4+1
 
     def forward1(self, x):
         z1 = self.conv1(x)
-        z1 = torch.cat((z1,-z1),dim=1)
+        z1 = torch.cat((z1,-z1[:,1:,:,:]),dim=1)
         A1 = F.relu(z1)
         return A1
     
     def forward2(self,A1):
         z2 = self.conv2(A1)
-        z2 = torch.cat((z2,-z2),dim=1)
+        z2 = torch.cat((z2,-z2[:,1:,:,:]),dim=1)
         A2 = F.relu(z2)
         return A2
         
     def forward3(self,A2):
         z3 = self.conv3(A2)
-        z3 = torch.cat((z3,-z3),dim=1)
+        z3 = torch.cat((z3,-z3[:,1:,:,:]),dim=1)
         A3 = F.relu(z3)
         return A3
     
     def forward4(self,A3):
         z4 = self.conv4(A3)
-        z4 = torch.cat((z4,-z4),dim=1)
+        z4 = torch.cat((z4,-z4[:,1:,:,:]),dim=1)
         A4 = F.relu(z4)
+        A4 = np.cat((z4[:,0,:,:],A4),dim=1)
         return A4
     
     def forward5(self,A4):
         z5 = self.conv5(A4)
-        z5 = torch.cat((z5,-z5),dim=1)
+        z5 = torch.cat((z5,-z5[:,1:,:,:]),dim=1)
         A5 = F.relu(z5)
         return A5
 
@@ -115,21 +172,21 @@ print('processing A1 ...')
 X=torch.Tensor(X)
 X=Variable(X)
 if args.weight_INT == 'pca':
-    W_pca1=np.load('/home/yifang/SAAK_superResolution/PCA_weight/PCA_weight1_12_3_2_2.npy')
+    W_pca1=np.load('/home/yifang/SAAK_superResolution/PCA_weight/PCA_weight1_12_3_2_2_04.npy')
     net.conv1.weight.data=torch.Tensor(W_pca1)
 net.conv1.bias.data.fill_(0)
 A1 = net.forward1(X)
 
 print('processing A2 ...')
 if args.weight_INT == 'pca':
-    W_pca2=np.load('/home/yifang/SAAK_superResolution/PCA_weight/PCA_weight1_96_24_2_2.npy')
+    W_pca2=np.load('/home/yifang/SAAK_superResolution/PCA_weight/PCA_weight1_96_24_2_2_04.npy')
     net.conv2.weight.data=torch.Tensor(W_pca2)
 net.conv2.bias.data.fill_(0)
 A2 = net.forward2(A1)
 
 print('processing A3 ...')
 if args.weight_INT == 'pca':
-    W_pca3=np.load('/home/yifang/SAAK_superResolution/PCA_weight/PCA_weight1_768_192_2_2.npy')
+    W_pca3=np.load('/home/yifang/SAAK_superResolution/PCA_weight/PCA_weight1_768_192_2_2_04.npy')
     net.conv3.weight.data=torch.Tensor(W_pca3)
 net.conv3.bias.data.fill_(0)
 A3 = net.forward3(A2)
@@ -140,40 +197,48 @@ A3 = net.forward3(A2)
 #net.conv4.bias.data.fill_(0)
 #A4 = net.forward4(A3)
 
+showFeatureVec(A3)
+
+
 class Inv_Net(nn.Module):
 
     def __init__(self):
         super(Inv_Net, self).__init__()
         # 1 input image channel, 6 output channels, 5x5 square convolution
         # kernel
-        self.deconv1 = nn.ConvTranspose2d(12,3,2,stride=2 )
-        self.deconv2 = nn.ConvTranspose2d(96, 24, 2,stride=2)
-        self.deconv3 = nn.ConvTranspose2d(768, 192, 2,stride=2)
-        self.deconv4 = nn.ConvTranspose2d(768*8, 768, 2,stride=2)
-        self.deconv5 = nn.ConvTranspose2d(768*64, 768*8, 2,stride=2)
+        curL_in,curL_out = net.conv1.weight.size()[0],net.conv1.weight.size()[1];self.curL_in1 = curL_in
+        self.deconv1 = nn.ConvTranspose2d(curL_in-1,curL_out,2,stride=2 )
+        curL_in,curL_out = net.conv2.weight.size()[0],net.conv2.weight.size()[1];self.curL_in2 = curL_in
+        self.deconv2 = nn.ConvTranspose2d(curL_in-1,curL_out, 2,stride=2)
+        curL_in,curL_out = net.conv3.weight.size()[0],net.conv3.weight.size()[1];self.curL_in3 = curL_in
+        self.deconv3 = nn.ConvTranspose2d(curL_in-1,curL_out, 2,stride=2)
+        curL_in,curL_out = net.conv4.weight.size()[0],net.conv4.weight.size()[1];self.curL_in4 = curL_in
+        self.deconv4 = nn.ConvTranspose2d(curL_in-1,curL_out, 2,stride=2)
+        curL_in,curL_out = net.conv5.weight.size()[0],net.conv5.weight.size()[1];self.curL_in5 = curL_in
+        self.deconv5 = nn.ConvTranspose2d(curL_in-1,curL_out, 2,stride=2)
 
     def forward1_inv(self, A1):
-        z1 = A1[:,0:12,:,:]-A1[:,12:24,:,:] 
+        z1 = A1[:,1:self.curL_in1,:,:]-A1[:,self.curL_in1:,:,:] 
         x = self.deconv1(z1)
         return x
         
     def forward2_inv(self, A2):
-        z2 = A2[:,0:96,:,:]-A2[:,96:192,:,:] 
+        z2 = A2[:,1:self.curL_in2,:,:]-A2[:,self.curL_in2:,:,:] 
         A1 = self.deconv2(z2)
         return A1
     
     def forward3_inv(self,A3):
-        z3 = A3[:,0:768,:,:]-A3[:,768:768*2,:,:] 
+        z3 = A3[:,1:self.curL_in3,:,:]-A3[:,self.curL_in3:,:,:] 
         A2 = self.deconv3(z3)
         return A2
     
     def forward4_inv(self,A4):
-        z4 = A4[:,0:768*8,:,:]-A4[:,768*8:768*2*8,:,:] 
+        z4 = A4[:,1:self.curL_in4,:,:]-A4[:,self.curL_in4:,:,:] 
         A3 = self.deconv4(z4)
         return A3    
     
     def forward5_inv(self,A5):
-        z5 = A5[:,0:768*64,:,:]-A5[:,768*64:768*2*64,:,:] 
+        z5 = A5[:,1:self.curL_in5,:,:]-A5[:,self.curL_in5:,:,:] 
         A4 = self.deconv5(z5)
         return A4    
     
@@ -191,16 +256,18 @@ tic = time.clock()
 A3_back=A3
 print('processing A3 back to A2 ...')
 if args.weight_INT == 'pca':
-    net_inv.deconv3.weight.data=net.conv3.weight.data
+    net_inv.deconv3.weight.data=net.conv3.weight.data[1:,:,:,:]
 else:
     net_inv.deconv3.weight.data=torch.Tensor(Cal_inv_W(net.conv3.weight.data.numpy()))
 net_inv.deconv3.bias.data.fill_(0)
 A2_back = net_inv.forward3_inv(A3_back)
 
+
+#A2_back = A2
 print('processing A2 back to A1...')
 if args.weight_INT == 'pca':
     print('pca')
-    net_inv.deconv2.weight.data=net.conv2.weight.data
+    net_inv.deconv2.weight.data=net.conv2.weight.data[1:,:,:,:]
 else:
     net_inv.deconv2.weight.data=torch.Tensor(Cal_inv_W(net.conv2.weight.data.numpy()))
 net_inv.deconv2.bias.data.fill_(0)
@@ -209,7 +276,7 @@ A1_back = net_inv.forward2_inv(A2_back)
 #A1_back = A1
 print('processing A1 back to x ...')
 if args.weight_INT == 'pca':
-    net_inv.deconv1.weight.data=net.conv1.weight.data
+    net_inv.deconv1.weight.data=net.conv1.weight.data[1:,:,:,:]
 else:
     net_inv.deconv1.weight.data=torch.Tensor(Cal_inv_W(net.conv1.weight.data.numpy()))
 net_inv.deconv1.bias.data.fill_(0)
@@ -219,3 +286,4 @@ print(result_back.data[0,:,:,:])
 
 toc = time.clock()
 print("Total time is %f" %(toc-tic))
+showImg(result_back,1, 'result_back')

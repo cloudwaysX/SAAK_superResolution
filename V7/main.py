1bba50f09d7ae1f320a57fac2a064ba3200dddc7
cloudwaysX <yifang@usc.edu>
2018-01-12 21:55:41 -0800

    local and global cluster

--- V7/main.py
+++ V7/main.py
@@ -15,7 +15,9 @@ parser.add_argument("--input_size", default = 9, type = int, help = "input size 
 parser.add_argument("--stride", default = 9, type = int, help = "stride when cropping patches from original image")
 parser.add_argument("--down_scale", default = 3, type = int, help = "how much downscale you want to get for Low Resolution Image")
 ########clf parameter##########################
-parser.add_argument("--n_cluster1", default = 10, type=int, help = "how many clusters you want; if not using kmean, should be 1")
+parser.add_argument("--n_cluster_local", default = 10, type=int, help = "how many clusters you want; if not using kmean, should be 1")
+parser.add_argument("--n_cluster_global", default = 8, type=int, help = "how many clusters you want; if not using kmean, should be 1")
+
 
 opt = parser.parse_args()
 
@@ -26,81 +28,77 @@ myDataset = readDataset.DatasetBSD()
 myDataset.readBSD_fromMatlab(inputSize=opt.input_size,stride = opt.stride,scale=opt.down_scale)
 trainset = myDataset.loadData(dataset = 'training')
 
-def KmeanHelper(input):    
+def KmeanHelper(input,n_clusters):    
     from sklearn.cluster import KMeans
-    k_means = KMeans(n_clusters = opt.n_cluster1,max_iter=300,n_jobs=6,random_state=0)
+    k_means = KMeans(n_clusters = n_clusters,max_iter=300,n_jobs=6,random_state=0)
     k_means.fit(input)
     kmeans_D = k_means.transform(input)
     return k_means,kmeans_D
-
-def showImage(imgBatch_LR,imgBatch_HR,index=0):
+    
+def checkCluster(n_clusters,imgBatch_LR,imgBatch_HR,labels,kmeansD):
     import matplotlib.pyplot as plt
-    
-    ax = plt.subplot("141")
-    tmp = np.clip(imgBatch_HR[index],0,255)
-    ax.imshow(tmp[0],cmap='gray')
-    ax.set_title("HR {}".format(index))
-
-    ax = plt.subplot("142")
-    tmp = np.clip(imgBatch_LR[index],0,255)
-    ax.imshow(tmp[0],cmap='gray')
-    ax.set_title("Input(bicubic)")
-    
-    ax = plt.subplot("143")
-    tmp = np.clip(imgBatch_HR[index]-imgBatch_LR[index],0,255)
-    ax.imshow(tmp[0],cmap='gray')
-    ax.set_title("diff")
-    
-    plt.show()
-    
-def checkCluster(clusterI,imgBatch_LR,imgBatch_HR,labels,kmeansD):
-    from sklearn.preprocessing import scale
+    plt.figure(figsize=(20,35))
     
     size = imgBatch_HR.shape[0]
-    count = 0
     
-    meanD = np.mean(kmeansD[:,clusterI][labels==clusterI])
-    for i in range(size):
-        if labels[i]==clusterI and (kmeansD[:,clusterI][i]<=meanD*0.7):
-            print('distance is {}'.format(kmeansD[:,clusterI][i]))
-            print('var for distance is {}'.format(scale(np.var(kmeansD,axis=1))[i]))
-            showImage(imgBatch_LR,imgBatch_HR,index=i)
-            count +=1
-        if count == 5: break
-    
+    for clusterI in range(n_clusters):
+        count = 0
+        meanD = np.mean(kmeansD[:,clusterI][labels==clusterI])    
+        for i in range(size):
+            if labels[i]==clusterI and (kmeansD[:,clusterI][i]<=meanD*0.6):
+            
+                ax = plt.subplot(n_clusters,12,1+count+12*clusterI)
+                tmp = np.clip(imgBatch_LR[i],0,255)
+                ax.imshow(tmp[0],cmap='gray')
+                ax.set_title("LR")
+                
+                ax = plt.subplot(n_clusters,12,count+7+12*clusterI)
+                tmp = np.clip(imgBatch_HR[i],0,255)
+                ax.imshow(tmp[0],cmap='gray')
+                ax.set_title("HR {}".format(i))
+                
+                count +=1
+            if count == 6: break
+            
 def createLocalBlocks(input):
     from skimage.util.shape import view_as_windows
     import numpy as np
     
     assert len(input.shape) == 4, 'need input dimension = 4'
-    trainset_local = np.transpose(input,(2,3,0,1))
-    trainset_local = np.reshape(trainset_local,(9,9,trainset_local.shape[2]*trainset_local.shape[3]))
-    trainset_local = view_as_windows(trainset_local, window_shape=(3,3,1),step=(3,3,1))
-    patchNum = trainset_local.shape[0]*trainset_local.shape[1]*trainset_local.shape[2]
-    trainset_local = np.reshape(trainset_local, (int(patchNum), 9))
-    #Get the first Gradient
-    trainset_local = trainset_local-trainset_local[:,[4]]
+    local = np.transpose(input,(2,3,0,1))
+    local = np.reshape(local,(9,9,local.shape[2]*local.shape[3]))
+    local = view_as_windows(local, window_shape=(3,3,1),step=(3,3,1))
+    patchNum = local.shape[0]*local.shape[1]*local.shape[2]
+    local = np.reshape(local,(patchNum,1,3,3))
     
-    return trainset_local
+    return local
 
 def createGlobalSamples(input):
     from skimage.transform import downscale_local_mean
-    trainset_global = downscale_local_mean(input,(1,1,3,3))
-    trainset_global = np.reshape(trainset_global,(trainset_global.shape[0]*trainset_global.shape[1],trainset_global.shape[2]*trainset_global.shape[3]))
-    return trainset_global
+    myGlobal = downscale_local_mean(input,(1,1,3,3))
+    tmp = myGlobal
+    myGlobal = np.reshape(myGlobal,(myGlobal.shape[0]*myGlobal.shape[1],myGlobal.shape[2]*myGlobal.shape[3]))
+#    myGlobal = scale(myGlobal)
+    return tmp,myGlobal
 
-trainset_LR = trainset['LR_scale_{}_interpo'.format(opt.down_scale)][:1000]
-trainset_HR = trainset['HR'][:1000]
+trainset_LR = trainset['LR_scale_{}_interpo'.format(opt.down_scale)]
+trainset_HR = trainset['HR']
 
+trainset_LR_local = createLocalBlocks(trainset_LR)
+trainset_HR_local = createLocalBlocks(trainset_HR)
 
+#Get the first Gradient and put into Kmeans
+patchNum = trainset_LR_local.shape[0]
+trainset_LR_local2 = np.reshape(trainset_LR_local, (int(patchNum), 9))
+trainset_LR_local2 = trainset_LR_local2-trainset_LR_local2[:,[4]]
+kmeans_local,Ds_local = KmeanHelper(trainset_LR_local2,opt.n_cluster_local)
+checkCluster(opt.n_cluster_local,trainset_LR_local,trainset_HR_local,kmeans_local.labels_,kmeansD=Ds_local)
 
-#kmeans,Ds = KmeanHelper(np.concatenate((trainset_LR,trainset_HR),axis=1))
-#trainset_HR_local = np.reshape(trainset_HR,(trainset_HR.shape[0],1,3,3))
-#trainset_LR_local = np.reshape(trainset_LR,(trainset_HR.shape[0],1,3,3))
-#checkCluster(1,trainset_LR_local,trainset_HR_local,kmeans.labels_,kmeansD=Ds)
-
-
-
-trainset_LR_global = createGlobalSamples(trainset_LR)
-kmeans_global,Ds_global = KmeanHelper(trainset_LR_global)
-checkCluster(1,trainset_LR,trainset_HR,kmeans_global.labels_,kmeansD=Ds_global)
+tmp,trainset_LR_global = createGlobalSamples(trainset_LR)
+#trainset_LR_global[:,4]=trainset_LR_global[:,4]*2; 
+trainset_LR_global = trainset_LR_global - trainset_LR_global[:,[4]]
+#from sklearn.preprocessing import scale
+#trainset_LR_global = scale(trainset_LR_global)
+kmeans_global,Ds_global = KmeanHelper(trainset_LR_global,opt.n_cluster_global)
+checkCluster(opt.n_cluster_global,trainset_LR,trainset_HR,kmeans_global.labels_,kmeansD=Ds_global)
+#checkCluster(opt.n_cluster_global,tmp,trainset_HR,kmeans_global.labels_,kmeansD=Ds_global)
